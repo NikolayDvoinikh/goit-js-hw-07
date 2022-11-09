799d9d8f7eebdf8556e7eabe70c72cd84fc4d7b7
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2022-11-09 16:01:53 +0200

    Task1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector("div.gallery");
+const galleryList = createGalleryItems(galleryItems);
+
+function createGalleryItems(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="Image ${description}"
+                />
+            </a>
+        </div>
+        `;
+		})
+		.join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryList);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(e) {
+	if (!e.target.classList.contains("gallery__image")) {
+		return;
+	}
+	e.preventDefault();
+	const instance = basicLightbox.create(`<img
+                    src="${e.target.dataset.source}"
+                    />`);
+	instance.show();
+
+	galleryContainer.addEventListener("keydown", e => {
+		if (e.code === "Escape") {
+			instance.close();
+		}
+	});
+}
